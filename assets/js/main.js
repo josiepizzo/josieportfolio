50bac0d95299dc66a8cc97d85879e43c6a3a37f8
Josephine Pizzolato <josiepizzo@gmail.com>
2019-08-26 22:12:14 -0400

    update for accessibility

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,29 +1,29 @@
-$(function(){
+$(function() {
 
     /*-------------------------------------------------------------------*/
     /*  1. Preloader. Requires jQuery jpreloader plugin.
     /*-------------------------------------------------------------------*/
     $(document).ready(function() {
-        if ($.fn.jpreLoader){
+        if ($.fn.jpreLoader) {
             $('body').jpreLoader({
                 showPercentage: false,
                 loaderVPos: '50%'
             });
         }
     });
-    
+
 
     /*-------------------------------------------------------------------*/
     /*  2. Makes the height of all selected elements (".match-height")
     /*  exactly equal. Requires jQuery matchHeight plugin.
     /*-------------------------------------------------------------------*/
-    $(window).smartload(function(){
-        if ($.fn.matchHeight){
+    $(window).smartload(function() {
+        if ($.fn.matchHeight) {
             $('.match-height').matchHeight();
         }
     });
-    
-    
+
+
     /*-------------------------------------------------------------------*/
     /*  3. Full screen
     /*-------------------------------------------------------------------*/
@@ -31,121 +31,230 @@ $(function(){
         var minHeight = $(window).height();
         $('.full-screen').css('min-height', minHeight);
     }
-    
+
     setResizeContent();
 
-    $(window).smartresize(function(){
+    $(window).smartresize(function() {
         setResizeContent();
     });
 
-    
+
     /*-------------------------------------------------------------------*/
     /*  4. Just did another hack of dropdown menu for Bootstrap scrollspy.
     /*-------------------------------------------------------------------*/
-    $('body').on('activate.bs.scrollspy', function(){
-        $('.page-scroll.dropdown > .dropdown-toggle').each(function(){
+    $('body').on('activate.bs.scrollspy', function() {
+        $('.page-scroll.dropdown > .dropdown-toggle').each(function() {
             $(this).attr('data-target', '#');
         });
     });
-    
-    
+
+
     /*-------------------------------------------------------------------*/
     /*  5. Page scrolling feature, requires jQuery Easing plugin.
     /*-------------------------------------------------------------------*/
-    var pageScroll = function(){
-        $('.page-scroll > a').bind('click', function(e){
+    var pageScroll = function() {
+        $('.page-scroll > a').bind('click', function(e) {
             e.preventDefault();
-            
+
             var anchor = $(this),
-            href = anchor.attr('href'),
-            offset = $('body').attr('data-offset');
-            
+                href = anchor.attr('href'),
+                offset = $('body').attr('data-offset');
+
             $('html, body').stop().animate({
                 scrollTop: $(href).offset().top - (offset - 1)
             }, 1500, 'easeInOutExpo');
-            
+
             /*
              * Automatically retract the navigation after clicking 
              * on one of the menu items.
              */
-            if(!$(this).parent().hasClass('dropdown')){
+            if (!$(this).parent().hasClass('dropdown')) {
                 $('.berg-collapse').collapse('hide');
             }
         });
     };
-    
+
     pageScroll();
-    
-    
+
+
     /*-------------------------------------------------------------------*/
     /*  6. Make navigation menu on your page always stay visible.
     /*  Requires jQuery Sticky plugin.
     /*-------------------------------------------------------------------*/
-    var stickyMenu = function(){
+    var stickyMenu = function() {
         var ww = Math.max($(window).width(), window.innerWidth),
-        nav = $('.navbar.navbar-fixed-top');
+            nav = $('.navbar.navbar-fixed-top');
 
-        if ($.fn.unstick){
+        if ($.fn.unstick) {
             nav.unstick();
         }
-        
-        if ($.fn.sticky && ww >= 992){
-            nav.sticky({topSpacing: 0});
+
+        if ($.fn.sticky && ww >= 992) {
+            nav.sticky({ topSpacing: 0 });
         }
     };
-    
+
     stickyMenu();
-    
+
     // Call stickyMenu() when window is resized.
-    $(window).smartresize(function(){
+    $(window).smartresize(function() {
         stickyMenu();
     });
-    
-    
+
+
+
+
+
+    /*-------------------------------------------------------------------*/
+    /*  JPizzolato - 7a. New code for toggle on education and work experience -
+    /*   Navbar dropdown opening on hover,
+    /*  and opening on click for collapsed navbar.
+    /*-------------------------------------------------------------------*/
+    $(document).ready(function() {
+
+        var hs1 = new hideShow('button1');
+        var hs2 = new hideShow('button2');
+        var hs3 = new hideShow('button3');
+        var hs4 = new hideShow('button4');
+
+    }); // end ready()
+
+    //
+    // function hideShow() is the constructor for a hideShow widget. it accepts the html ID of
+    // an element to attach to.
+    //
+    // @param(id string) id is the html ID of the element to attach to
+    //
+    // @return N/A
+    //
+    function hideShow(id) {
+
+        this.$id = $('#' + id);
+        this.$region = $('#' + this.$id.attr('aria-controls'));
+
+        this.keys = {
+            enter: 13,
+            space: 32
+        };
+
+        this.toggleSpeed = 100;
+
+        // bind handlers
+        this.bindHandlers();
+
+    } // end hidShow() constructor
+
+    //
+    // Function bindHandlers() is a member function to bind event handlers to the hideShow region
+    //
+    // return N/A
+    //
+    hideShow.prototype.bindHandlers = function() {
+
+        var thisObj = this;
+
+        this.$id.click(function(e) {
+
+            thisObj.toggleRegion();
+
+            e.stopPropagation();
+            return false;
+        });
+    }
+
+    //
+    // Function toggleRegion() is a member function to toggle the display of the hideShow region
+    //
+    // return N/A
+    //
+    hideShow.prototype.toggleRegion = function() {
+
+            var thisObj = this;
+
+            // toggle the region
+            this.$region.slideToggle(this.toggleSpeed, function() {
+
+                if ($(this).attr('aria-expanded') == 'false') { // region is collapsed
+
+                    // update the aria-expanded attribute of the region
+                    $(this).attr('aria-expanded', 'true');
+
+                    // move focus to the region
+                    $(this).focus();
+
+                    // update the button label
+                    thisObj.$id.find('span').html('Hide');
+
+                } else { // region is expanded
+
+                    // update the aria-expanded attribute of the region
+                    $(this).attr('aria-expanded', 'false');
+
+                    // update the button label
+                    thisObj.$id.find('span').html('Show');
+                }
+            });
+
+        } // end toggleRegion()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
     /*-------------------------------------------------------------------*/
     /*  7. Navbar dropdown opening on hover,
     /*  and opening on click for collapsed navbar.
     /*-------------------------------------------------------------------*/
-    var toggleNavbarMethod = function(){
+    var toggleNavbarMethod = function() {
         var ww = Math.max($(window).width(), window.innerWidth),
-        dropdown = $('.navbar .dropdown');
-        
-        if (ww >= 992){
-            dropdown.on('mouseover', function(){
-                if (!$(this).hasClass('open')){
+            dropdown = $('.navbar .dropdown');
+
+        if (ww >= 992) {
+            dropdown.on('mouseover', function() {
+                if (!$(this).hasClass('open')) {
                     $(this).addClass('open');
                 }
-            }).on('mouseout', function(){
-                if ($(this).hasClass('open')){
+            }).on('mouseout', function() {
+                if ($(this).hasClass('open')) {
                     $(this).removeClass('open');
                 }
             });
-        }
-        else {
+        } else {
             dropdown.off('mouseover').off('mouseout');
         }
     };
-    
+
     toggleNavbarMethod();
-    
+
     // Call toggleNavbarMethod(); when window is resized.
-    $(window).smartresize(function(){
+    $(window).smartresize(function() {
         toggleNavbarMethod();
     });
-    
-    
+
+
     /*-------------------------------------------------------------------*/
     /*  8. Prevent bootstrap dropdown closing when clicked.
     /*-------------------------------------------------------------------*/
-    $('.dropdown-menu').click(function(e){
+    $('.dropdown-menu').click(function(e) {
         e.stopPropagation();
     });
-    
- 
+
+
     /*-------------------------------------------------------------------*/
     /*  9. Portfolio gallery. Requires jQuery Magnific Popup plugin.
     /*-------------------------------------------------------------------*/
-    if ($.fn.magnificPopup){
+    if ($.fn.magnificPopup) {
         $('.portfolio').magnificPopup({
             delegate: 'a.zoom',
             type: 'image',
@@ -159,191 +268,187 @@ $(function(){
 
             gallery: {
                 enabled: true,
-                preload: [0,2],
+                preload: [0, 2],
                 arrowMarkup: '<button title="%title%" type="button" class="mfp-arrow mfp-arrow-%dir%"></button>',
                 tPrev: 'Previous Project',
                 tNext: 'Next Project'
             }
         });
     }
-    
-    
+
+
     /*-------------------------------------------------------------------*/
     /*  10. Column Chart (Section - My Strengths)
     /*-------------------------------------------------------------------*/
-    var columnChart = function (){
-        $('.column-chart').find('.item-progress').each(function(){
+    var columnChart = function() {
+        $('.column-chart').find('.item-progress').each(function() {
             var itemProgress = $(this);
             var itemProgressHeight = $(this).parent().height() * ($(this).data('percent') / 100);
-            
+
             itemProgress.css('height', itemProgressHeight);
         });
     };
-    
+
     // Call columnChart() when window is loaded.
-    $(window).smartload(function(){
+    $(window).smartload(function() {
         columnChart();
     });
-    
-    
+
+
     /*-------------------------------------------------------------------*/
     /*  11. Section - My Resume
     /*-------------------------------------------------------------------*/
-    var resumeCollapse = function (){
+    var resumeCollapse = function() {
         var ww = Math.max($(window).width(), window.innerWidth),
-        workItem = $('.collapse:not(:first)', '#work'),
-        educationItem = $('.collapse:not(:first)', '#education');
-        
-        if (ww < 768){
+            workItem = $('.collapse:not(:first)', '#work'),
+            educationItem = $('.collapse:not(:first)', '#education');
+
+        if (ww < 768) {
             workItem.collapse('show');
             educationItem.collapse('show');
-        }
-        else{
+        } else {
             workItem.collapse('hide');
             educationItem.collapse('hide');
         }
     };
-    
+
     // Call resumeCollapse() when window is loaded.
-    $(window).smartload(function(){
+    $(window).smartload(function() {
         resumeCollapse();
     });
-    
+
     // Call resumeCollapse() when window is resized.
-    $(window).smartresize(function(){
+    $(window).smartresize(function() {
         resumeCollapse();
     });
-    
-    
+
+
     /*-------------------------------------------------------------------*/
     /*	12. References slider. Requires Flexslider plugin.
     /*-------------------------------------------------------------------*/
-    $(window).smartload(function(){
-        if ($.fn.flexslider){
+    $(window).smartload(function() {
+        if ($.fn.flexslider) {
             var flex = $('.flexslider.references');
-    
+
             flex.flexslider({
                 selector: ".slides > .item",
                 manualControls: ".flex-control-nav li",
-                directionNav : false,
+                directionNav: false,
                 slideshowSpeed: 4000,
-                after: function(slider){
+                after: function(slider) {
                     if (!slider.playing) {
                         slider.play();
                     }
                 }
-            }); 
+            });
         }
     });
-    
-    $('a.flex-prev').on('click', function(e){
+
+    $('a.flex-prev').on('click', function(e) {
         e.preventDefault();
         $('.flexslider').flexslider('prev');
     });
-    
-    $('a.flex-next').on('click', function(e){
+
+    $('a.flex-next').on('click', function(e) {
         e.preventDefault();
         $('.flexslider').flexslider('next');
     });
-    
-    
+
+
     /*-------------------------------------------------------------------*/
     /*  13. Circle Chart (Section - Skills & Expertise)
     /*-------------------------------------------------------------------*/
-    var circleChart = function (){
-        $('.circle-chart').find('.item-progress').each(function(){
+    var circleChart = function() {
+        $('.circle-chart').find('.item-progress').each(function() {
             var item = $(this),
-            maxHeight = 108,
-            newHeight = maxHeight * ($(this).data('percent') / 100);
-            
+                maxHeight = 108,
+                newHeight = maxHeight * ($(this).data('percent') / 100);
+
             // Only animate elements when using non-mobile devices    
-            if (jQuery.browser.mobile === false){
+            if (jQuery.browser.mobile === false) {
                 item.one('inview', function(isInView) {
-                    if (isInView){
+                    if (isInView) {
                         // Animate item
                         item.animate({
                             height: newHeight
-                        },1500);
+                        }, 1500);
                     }
                 });
-            }
-            else{
+            } else {
                 item.css('height', newHeight);
             }
         });
     };
-    
+
     // Call circleChart() when window is loaded.
-    $(window).smartload(function(){
+    $(window).smartload(function() {
         circleChart();
     });
-    
-    
+
+
     /*-------------------------------------------------------------------*/
     /*  14. Bar Chart (Section - Knowledge)
     /*-------------------------------------------------------------------*/
-    var barChart = function (){
-        $('.bar-chart').find('.item-progress').each(function(){
+    var barChart = function() {
+        $('.bar-chart').find('.item-progress').each(function() {
             var item = $(this),
-            percent = $(this).prev(),
-            newWidth = $(this).parent().width() * ($(this).data('percent') / 100);
-            
+                percent = $(this).prev(),
+                newWidth = $(this).parent().width() * ($(this).data('percent') / 100);
+
             // Only animate elements when using non-mobile devices    
-            if (jQuery.browser.mobile === false){
+            if (jQuery.browser.mobile === false) {
                 item.one('inview', function(isInView) {
-                    if (isInView){
+                    if (isInView) {
                         // Animate item
                         item.animate({
                             width: newWidth
-                        },1500);
-                        
+                        }, 1500);
+
                         percent.animate({
                             left: newWidth - percent.width()
-                        },1500);
+                        }, 1500);
                     }
                 });
-            }
-            else{
+            } else {
                 item.css('width', newWidth);
                 percent.css('left', newWidth - percent.width());
             }
         });
     };
-    
+
     // Call barChart() when window is loaded.
-    $(window).smartload(function(){
+    $(window).smartload(function() {
         barChart();
     });
-    
+
     // Call barChart() when window is resized.
-    $(window).smartresize(function(){
+    $(window).smartresize(function() {
         barChart();
     });
-    
-    
+
+
     /*-------------------------------------------------------------------*/
     /*  15. Milestones counter.
     /*-------------------------------------------------------------------*/
-    var counter = function (){
+    var counter = function() {
         var number = $('.milestones').find('.number');
-        
-        if ($.fn.countTo){
+
+        if ($.fn.countTo) {
             number.countTo({
                 speed: 3000
             });
         }
     };
-    
-    if (jQuery.browser.mobile === false){
+
+    if (jQuery.browser.mobile === false) {
         var number = $('.milestones .number');
-        
+
         number.one('inview', function(isInView) {
-            if (isInView){
+            if (isInView) {
                 counter();
             }
         });
-    }
-    else{
+    } else {
         counter();
     }
 });
